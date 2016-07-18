8f54dd911252fe81f62dc5f9ee7d7db827e6cdb4
oolojedeASC <olojedejide@gmail.com>
2016-07-17 20:06:06 -0700

    Add files via upload

---
+++ movienamemaker.py.py
@@ -0,0 +1,39 @@
+# This code will create random movie names for a person.
+
+from random import *
+
+#The Lists
+firstlist = ["Fall of the ", "Carry ", "Pursuit ", "Finding ", "Power "
+            "Heroes ", "Butterman ", "Larry ", "Life of ", "Berry "]
+            
+secondlist = [ "Dragon", " Killer Warrior", "Courage ", "Pikachu ", "Rabbitman ", "Invisible Man"
+                " Mighty Guy ", "Hurkania " "Mirror rorriM ", "Night "]
+                
+thirdlist = [ "Cloud ", "Pokemoon ", "Search ", "Nighty ", "Shakey ", "Time ", "People ", 
+                "Unitiled ", "Compisio ", "Como Estas "]
+           
+#Start of the loop
+## name_list = ["Bob", "Charlie", "Ken", "Barbie", "Shantua"]
+## result_name = name_list[randrange(0, 4)]
+
+Resultf = firstlist[randrange(0,9)]
+Results = secondlist[randrange(0,9)]
+Resultt = thirdlist[randrange(0,9)]
+
+'''
+i = 0
+for i in firstlist:
+  Resultf
+p = 0
+for p in secondlist :
+ Results
+k = 0
+for k in thirdlist :
+   Resultt
+   '''
+sumofvari =  Resultf + Results + Resultt
+moviename = sumofvari
+print (moviename)
+              
+                
+# Start of the 2nd Loop
